5f307d53fe3fa2c521e3a69f6c319a7da51880fd
Khairul Hidayat <contact@khairul.my.id>
2020-03-19 23:53:03 +0700

    Added patients overview

---
+++ lib/chart.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+class ChartValue {
+  final int key;
+  final int value;
+
+  ChartValue(this.key, this.value);
+}
+
+class ChartSeries {
+  final String id;
+  final List<ChartValue> values;
+  final Color color;
+
+  ChartSeries({this.id, @required this.values, this.color = Colors.blue});
+}
+
+class TimeChart extends StatelessWidget {
+  final String title;
+  final double height;
+  final List<ChartSeries> series;
+  final String leftText;
+  final int timeSpan;
+
+  TimeChart({
+    this.title,
+    this.height = 250,
+    @required this.series,
+    this.leftText,
+    this.timeSpan = 0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    List<charts.Series<ChartValue, int>> seriesList = series.map((ChartSeries series) {
+      return charts.Series<ChartValue, int>(
+        id: series.id,
+        data: series.values,
+        colorFn: (_, __) => charts.ColorUtil.fromDartColor(series.color),
+        domainFn: (ChartValue item, _) => item.key,
+        measureFn: (ChartValue item, _) => item.value,
+      );
+    }).toList();
+
+    List<charts.ChartBehavior> behaviors = [
+      charts.ChartTitle(
+        title,
+        behaviorPosition: charts.BehaviorPosition.top,
+        titleOutsideJustification: charts.OutsideJustification.start,
+        innerPadding: 32,
+        titleStyleSpec: charts.TextStyleSpec(fontSize: 16)
+      ),
+      charts.ChartTitle(
+        "Jam",
+        behaviorPosition: charts.BehaviorPosition.bottom,
+        titleStyleSpec: charts.TextStyleSpec(fontSize: 12)
+      ),
+      charts.ChartTitle(
+        leftText,
+        behaviorPosition: charts.BehaviorPosition.start,
+        titleStyleSpec: charts.TextStyleSpec(fontSize: 12)
+      ),
+    ];
+
+    if (series.length > 1) {
+      behaviors.add(charts.SeriesLegend(position: charts.BehaviorPosition.bottom));
+    }
+
+    return SizedBox(
+      height: height,
+      child: charts.LineChart(
+        seriesList,
+        defaultRenderer: charts.LineRendererConfig(includePoints: true),
+        domainAxis: charts.NumericAxisSpec(
+          tickProviderSpec: charts.BasicNumericTickProviderSpec(desiredTickCount: 9),
+          tickFormatterSpec: charts.BasicNumericTickFormatterSpec((num value) {
+            int time = value.toInt() + timeSpan;
+            return (time % 2 == 0)
+            ? time.toString().padLeft(2, '0')
+            : "";
+          })
+        ),
+        behaviors: behaviors,
+        animate: true,
+      )
+    );
+  }
+}
--- lib/view.dart
+++ lib/view.dart
@@ -1,6 +1,239 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:tanda_vital_flutter/pasien.dart';
+import 'package:tanda_vital_flutter/chart.dart';
 import 'package:tanda_vital_flutter/utils.dart';
+
+class Overview extends StatelessWidget {
+  final PasienData pasien;
+
+  Overview({this.pasien});
+
+  Widget dataPasien() => Card(
+    child: Container(
+      padding: EdgeInsets.only(top: 16, right: 16, bottom: 16),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            flex: 6,
+            child: Icon(MdiIcons.accountCircle, size: 48)
+          ),
+          Expanded(
+            flex: 9,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('No. RM', style: TextStyle(fontSize: 12, color: Colors.black45)),
+                Container(
+                  margin: EdgeInsets.only(top: 2),
+                  child: Text(pasien.regNumber)
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 12),
+                  child: Text('Nama', style: TextStyle(fontSize: 12, color: Colors.black45))
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 2),
+                  child: Text(pasien.name)
+                ),
+              ],
+            )
+          ),
+          Expanded(
+            flex: 9,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('Umur', style: TextStyle(fontSize: 12, color: Colors.black45)),
+                Container(
+                  margin: EdgeInsets.only(top: 2),
+                  child: Text(pasien.age.toString() + ' th')
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 12),
+                  child: Text('Jenis Kelamin', style: TextStyle(fontSize: 12, color: Colors.black45))
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 2),
+                  child: Text(pasien.gender == 1 ? 'Perempuan' : 'Laki-laki')
+                ),
+              ],
+            )
+          ),
+        ],
+      ),
+    )
+  );
+
+  Widget chartTD() => Card(
+    child: Container(
+      width: double.maxFinite,
+      padding: EdgeInsets.all(8),
+      child: TimeChart(
+        title: "Tekanan Darah",
+        leftText: "mmHg",
+        series: <ChartSeries>[
+          ChartSeries(
+            id: 'Sistolik',
+            color: Colors.red.shade900,
+            values: <ChartValue>[
+              ChartValue(0, 110),
+              ChartValue(1, 120),
+              ChartValue(2, 180),
+            ],
+          ),
+          ChartSeries(
+            id: 'Diastolik',
+            color: Colors.red.shade300,
+            values: <ChartValue>[
+              ChartValue(0, 70),
+              ChartValue(1, 80),
+              ChartValue(2, 100),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
+
+  Widget chartNadi(List<ChartValue> values) => Card(
+    child: Container(
+      width: double.maxFinite,
+      padding: EdgeInsets.all(8),
+      child: TimeChart(
+        title: "Denyut Nadi",
+        leftText: "x/menit",
+        series: <ChartSeries>[
+          ChartSeries(
+            id: 'HR',
+            color: Colors.green,
+            values: values,
+          ),
+        ],
+      ),
+    ),
+  );
+
+  Widget buildChart({
+    title, leftText, series, timeSpan
+  }) => Card(
+    child: Container(
+      width: double.maxFinite,
+      padding: EdgeInsets.all(8),
+      child: TimeChart(
+        title: title,
+        leftText: leftText,
+        series: series,
+        timeSpan: timeSpan,
+      ),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final chartValues = [
+      {
+        'waktu': 14,
+        'sistol': 110,
+        'diastol': 70,
+        'nadi': 88,
+        'rr': 21
+      },
+      {
+        'waktu': 15,
+        'sistol': 100,
+        'diastol': 60,
+        'nadi': 82,
+        'rr': 20
+      },
+      {
+        'waktu': 16,
+        'sistol': 120,
+        'diastol': 70,
+        'nadi': 86,
+        'rr': 18
+      },
+      {
+        'waktu': 18,
+        'sistol': 110,
+        'diastol': 60,
+        'nadi': 72,
+        'rr': 18
+      },
+    ];
+
+    List<ChartValue> sistolik = [];
+    List<ChartValue> diastolik = [];
+    List<ChartValue> nadi = [];
+    List<ChartValue> rr = [];
+    int timeSpan = 0;
+
+    chartValues.asMap().forEach((index, item) {
+      if (index == 0) {
+        timeSpan = item['waktu'];
+      }
+      sistolik.add(ChartValue(item['waktu'] - timeSpan, item['sistol']));
+      diastolik.add(ChartValue(item['waktu'] - timeSpan, item['diastol']));
+      nadi.add(ChartValue(item['waktu'] - timeSpan, item['nadi']));
+      rr.add(ChartValue(item['waktu'] - timeSpan, item['rr']));
+    });
+
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          dataPasien(),
+
+          // Chart tekanan darah
+          buildChart(
+            title: "Tekanan Darah",
+            leftText: "mmHg",
+            timeSpan: timeSpan,
+            series: <ChartSeries>[
+              ChartSeries(
+                id: 'Sistolik',
+                color: Colors.red.shade900,
+                values: sistolik,
+              ),
+              ChartSeries(
+                id: 'Diastolik',
+                color: Colors.red.shade300,
+                values: diastolik,
+              ),
+            ],
+          ),
+
+          // Chart denyut nadi
+          buildChart(
+            title: "Denyut Nadi",
+            leftText: "x/menit",
+            timeSpan: timeSpan,
+            series: <ChartSeries>[
+              ChartSeries(
+                color: Colors.green,
+                values: nadi,
+              ),
+            ],
+          ),
+
+          // Chart pernafasan
+          buildChart(
+            title: "Pernafasan",
+            leftText: "x/menit",
+            timeSpan: timeSpan,
+            series: <ChartSeries>[
+              ChartSeries(
+                color: Colors.blue,
+                values: rr,
+              ),
+            ],
+          ),
+        ],
+      )
+    );
+  }
+}
 
 class LihatPasien extends StatefulWidget {
   final PasienData data;
@@ -11,14 +244,48 @@ class LihatPasien extends StatefulWidget {
   _LihatPasienState createState() => _LihatPasienState();
 }
 
-class _LihatPasienState extends State<LihatPasien> {
+class _LihatPasienState extends State<LihatPasien> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  final List<Tab> _tabs = <Tab>[
+    Tab(text: 'Ikhtisar'),
+    Tab(text: 'Riwayat'),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: _tabs.length, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  List<Widget> buildPages() {
+    final data = this.widget.data;
+    return <Widget>[
+      Overview(pasien: data),
+      Text('Haha'),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(Utils.genderPrefix(this.widget.data)),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: _tabs,
+        ),
       ),
-      body: Container(),
+      body: TabBarView(
+        controller: _tabController,
+        children: buildPages(),
+      ),
     );
   }
 }
